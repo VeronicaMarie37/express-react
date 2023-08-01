9e39309336a665dd8d3aec913ed37cdef07c9414
MelvinLewis <veronica.m.hannah@gmail.com>
2023-08-01 19:17:27 -0400

    full crud backend api

--- server.js
+++ server.js
@@ -7,22 +7,52 @@ require("dotenv").config();
 
 // pull PORT from .env, give default value of 4000 and establish DB Connection
 const { PORT, MONGODB_URI } = process.env;
+const mongoose = require('mongoose');
+
+
+// import people router
+const peopleController = require('./controllers/people-controller')
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+mongoose.connect(MONGODB_URI)
 
 // import express
 const express = require("express");
 
 // create application object
 const app = express();
-	
-	///////////////////////////////
+
+// import people router
+const cors = require("cors")
+const morgan = require("morgan")
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(express.json()); // parse json bodies - this will run before our request accesses the people router
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging for development
+// all requests for endpoints that begin with '/people'
+app.use('/people', peopleController)
+
+
+///////////////////////////////
 	// ROUTES
-	////////////////////////////////
+///////////////////////////////
 	// create a test route
-	app.get("/", (req, res) => {
-	    res.send("hello world");
-	});
+app.get("/", (req, res) => {
+res.send("hello world");
+});
 
 ///////////////////////////////
 // LISTENER
 ////////////////////////////////
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+
+
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
